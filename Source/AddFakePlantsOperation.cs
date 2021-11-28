f3bd3420e1dc869f3c026f896fd2843704cc4e55
Radek Falhar <Radek.Falhar@hotmail.com>
2021-11-28 10:34:59 +0100

    Initial commit

---
+++ Source/AddFakePlantsOperation.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Verse;
+// ReSharper disable InconsistentNaming
+
+namespace Euphoric.DecorativeGrassAndPlants
+{
+    // ReSharper disable once UnusedType.Global
+    public class AddFakePlantsOperation : PatchOperation
+    {
+        public enum PlantKind
+        {
+            Plant,
+            Tree,
+            Bush,
+            CavePlant
+        }
+
+        public class FakePlantVariant
+        {
+            public string DefName { get; }
+            public string Label { get; }
+            public float VisualSize { get; }
+            public int MaxMeshCount { get; }
+
+            public FakePlantVariant(string defName, string label, float visualSize, int maxMeshCount)
+            {
+                DefName = defName;
+                Label = label;
+                VisualSize = visualSize;
+                MaxMeshCount = maxMeshCount;
+            }
+        }
+
+        public class FakePlantDef
+        {
+            public string defName;
+            public string label;
+            public FloatRange plantVisualSizeRange = FloatRange.One;
+            public int maxMeshCount = 1;
+            public XmlElement graphicDataNode;
+            public PlantKind kind = PlantKind.Plant;
+            public float topWindExposure = 0.25f;
+
+            public FakePlantDef Copy()
+            {
+                return new FakePlantDef()
+                {
+                    defName = defName,
+                    label = label,
+                    plantVisualSizeRange = plantVisualSizeRange,
+                    maxMeshCount = maxMeshCount,
+                    kind = kind,
+                    topWindExposure = topWindExposure,
+                    graphicDataNode = graphicDataNode.CloneNode(true) as XmlElement
+                };
+            }
+
+
+            public IEnumerable<FakePlantVariant> CreateVariants()
+            {
+                foreach (var meshCountLabel in CreateMeshCountVariants())
+                {
+                    foreach (var sizeVariant in CreateSizeVariants())
+                    {
+                        var variantDefName = defName + "_Fake_" + sizeVariant.Item2 + "_" + meshCountLabel.Item2;
+                        var variantLabel = "fake " + label +" " + sizeVariant.Item3 + " " + meshCountLabel.Item3;
+
+                        yield return new FakePlantVariant(variantDefName, variantLabel, sizeVariant.Item1, meshCountLabel.Item1);
+                    }
+                }
+            }
+
+            private IEnumerable<(int, string, string)> CreateMeshCountVariants()
+            {
+                List<(int, string, string)> meshCountLabels = new List<(int, string, string)>()
+                {
+                    (1, "Single", "single"),
+                    (4, "Few", "few"),
+                    (9, "Some", "some"),
+                    (16, "Lots", "lots"),
+                    (25, "Many", "many"),
+                };
+
+                if (maxMeshCount == 1)
+                {
+                    yield return (1, "Single", "");
+                }
+                else
+                {
+                    foreach (var meshCountLabel in meshCountLabels)
+                    {
+                        if (meshCountLabel.Item1 > maxMeshCount)
+                            break;
+
+                        yield return meshCountLabel;
+                    }
+                }
+            }
+
+            private IEnumerable<(float, string, string)> CreateSizeVariants()
+            {
+                yield return (plantVisualSizeRange.TrueMin, "Small", "small");
+                if (plantVisualSizeRange.Span > 0.5)
+                {
+                    yield return (plantVisualSizeRange.Average, "Medium", "medium");
+                }
+                yield return (plantVisualSizeRange.TrueMax, "Large", "large");
+            }
+        }
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            Log.Warning("Applying fake plants patch");
+
+            var plantBase = xml.SelectNodes("/Defs/ThingDef[@Name='PlantBaseNonEdible']")[0];
+            var fakePlantDef = new FakePlantDef() { graphicDataNode = xml.CreateElement("graphicData") };
+            var fakePlantDefs = ProcessPlantXmlNode(xml, plantBase, fakePlantDef).ToList();
+
+            var logMessage = "Plant names:" + Environment.NewLine + PlantDefsDebug(fakePlantDefs);
+            Log.Warning(logMessage);
+
+            var defsNode = xml.SelectSingleNode("Defs");
+
+            foreach (var plantDef in fakePlantDefs)
+            {
+                var fakePlantDesignatorNode = CreateFakePlantDesignator(xml, plantDef);
+                defsNode.AppendChild(fakePlantDesignatorNode);
+
+                foreach (var variant in plantDef.CreateVariants())
+                {
+                    var fakePlantDefNode = CreateFakePlantDefNode(xml, plantDef, variant);
+                    defsNode.AppendChild(fakePlantDefNode);
+                }
+            }
+
+            return true;
+        }
+
+        private static XmlNode CreateFakePlantDesignator(XmlDocument xml, FakePlantDef plantDef)
+        {
+            var fakePlantXml =
+                $"<DesignatorDropdownGroupDef>" +
+                $"<defName>Decorations_Fake_{plantDef.defName}</defName>" +
+                $"</DesignatorDropdownGroupDef>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(fakePlantXml);
+
+            var fakePlantDefNode = xml.ImportNode(doc.FirstChild, true);
+            
+            return fakePlantDefNode;
+        }
+
+        private static XmlNode CreateFakePlantDefNode(XmlDocument xml, FakePlantDef plantDef, FakePlantVariant variant)
+        {
+            var fakePlantXml =
+                $"<ThingDef ParentName=\"FakePlantBase\">" +
+                $"<defName>{variant.DefName}</defName>" +
+                $"<label>{variant.Label}</label>" +
+                plantDef.graphicDataNode.OuterXml +
+                $"<modExtensions><li Class=\"Euphoric.DecorativeGrassAndPlants.FakePlantExtension\">" +
+                $"<visualSize>{variant.VisualSize}</visualSize>" +
+                $"<maxMeshCount>{variant.MaxMeshCount}</maxMeshCount>" +
+                $"<topWindExposure>{plantDef.topWindExposure}</topWindExposure>" +
+                $"</li></modExtensions>" +
+                $"<designatorDropdown>Decorations_Fake_{plantDef.defName}</designatorDropdown>" +
+                $"</ThingDef>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(fakePlantXml);
+
+            var fakePlantDefNode = xml.ImportNode(doc.FirstChild, true);
+            //Log.Message(fakePlantDefNode.OuterXml);
+            return fakePlantDefNode;
+        }
+
+        private static string PlantDefsDebug(List<FakePlantDef> fakePlantDefs)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var plantGroup in fakePlantDefs.GroupBy(x => x.kind))
+            {
+                sb.AppendLine(plantGroup.Key.ToString());
+                foreach (var plantDef in plantGroup)
+                {
+                    sb.AppendLine(
+                        $"  {plantDef.defName} {plantDef.label} {plantDef.kind} {plantDef.plantVisualSizeRange} {plantDef.plantVisualSizeRange.Span} {plantDef.maxMeshCount}");
+
+                    foreach (var variant in plantDef.CreateVariants())
+                    {
+                        sb.AppendLine($"    {variant.DefName} {variant.MaxMeshCount}");
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private IEnumerable<FakePlantDef> ProcessPlantXmlNode(XmlDocument xml, XmlNode node, FakePlantDef parentDef)
+        {
+            var thisDef = parentDef.Copy();
+
+            try
+            {
+                var plantNode = node.SelectSingleNode("plant");
+                if (plantNode != null)
+                {
+                    var visualSizeRangeNode = plantNode.SelectSingleNode("visualSizeRange");
+                    if (visualSizeRangeNode != null)
+                    {
+                        thisDef.plantVisualSizeRange = ParseFloatRangeFromXmlNode(visualSizeRangeNode);
+                    }
+
+                    var maxMeshCountNode = plantNode.SelectSingleNode("maxMeshCount");
+                    if (maxMeshCountNode != null)
+                    {
+                        thisDef.maxMeshCount = int.Parse(maxMeshCountNode.InnerText);
+                    }
+                    
+                    var topWindExposureNode = plantNode.SelectSingleNode("topWindExposure");
+                    if (topWindExposureNode != null)
+                    {
+                        thisDef.topWindExposure = float.Parse(topWindExposureNode.InnerText);
+                    }
+                }
+
+                if (node.SelectSingleNode("graphicData") is XmlElement graphicDataNode)
+                {
+                    MergeNodes(thisDef.graphicDataNode, graphicDataNode);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error parsing plant def {node.OuterXml} {e}");
+                yield break;
+            }
+
+            var name = node.Attributes["Name"]?.Value;
+            if (name != null)
+            {
+                if (name == "TreeBase")
+                {
+                    thisDef.kind = PlantKind.Tree;
+                }
+                else if (name == "BushBase")
+                {
+                    thisDef.kind = PlantKind.Bush;
+                }
+                else if (name == "CavePlantBase")
+                {
+                    thisDef.kind = PlantKind.CavePlant;
+                }
+                
+                // is parent, so go through children
+                var xpath = $"/Defs/ThingDef[@ParentName='{name}']";
+                var plantChildren = xml.SelectNodes(xpath);
+
+                foreach (XmlNode plantChild in plantChildren)
+                {
+                    var childDefs = ProcessPlantXmlNode(xml, plantChild, thisDef);
+                    foreach (var childDef in childDefs)
+                    {
+                        yield return childDef;
+                    }
+                }
+            }
+            else
+            {
+                // is leaf, return as completed plant def
+                thisDef.defName = node.SelectSingleNode("defName").InnerText;
+                thisDef.label = node.SelectSingleNode("label").InnerText;
+
+                yield return thisDef;
+            }
+        }
+
+        private static FloatRange ParseFloatRangeFromXmlNode(XmlNode visualSizeRangeNode)
+        {
+            FloatRange fr;
+
+            if (visualSizeRangeNode.ChildNodes.Count == 1)
+            {
+                fr = FloatRange.FromString(visualSizeRangeNode.InnerText);
+            }
+            else
+            {
+                fr = new FloatRange();
+                fr.min = float.Parse(visualSizeRangeNode.SelectSingleNode("min").InnerText);
+                fr.max = float.Parse(visualSizeRangeNode.SelectSingleNode("max").InnerText);
+            }
+
+            return fr;
+        }
+
+        private void MergeNodes(XmlElement targetNode, XmlElement sourceNode)
+        {
+            foreach (XmlElement childNode in sourceNode.ChildNodes)
+            {
+                var clonedChildNode = childNode.CloneNode(true);
+
+                var targetChildren = targetNode.SelectSingleNode(childNode.Name);
+                if (targetChildren == null)
+                {
+                    targetNode.AppendChild(clonedChildNode);
+                }
+                else
+                {
+                    targetNode.ReplaceChild(clonedChildNode, targetChildren);
+                }
+            }
+        }
+    }
+}
---
+++ Source/FakePlant.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Euphoric.DecorativeGrassAndPlants
+{
+    public class FakePlantExtension : DefModExtension
+    {
+        public float visualSize = 1;
+        public int maxMeshCount = 1;
+        public float topWindExposure = 0.25f;
+    }
+
+    [StaticConstructorOnStartup]
+    public class FakePlant : ThingWithComps
+    {
+        private string cachedLabelMouseover;
+        private static Color32[] workingColors = new Color32[4];
+
+        [TweakValue("Graphics", -1f, 1f)] private static float LeafSpawnRadius = 0.4f;
+        [TweakValue("Graphics", 0.0f, 2f)] private static float LeafSpawnYMin = 0.3f;
+        [TweakValue("Graphics", 0.0f, 2f)] private static float LeafSpawnYMax = 1f;
+
+        public override string LabelMouseover
+        {
+            get
+            {
+                if (cachedLabelMouseover == null)
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.Append((string)def.LabelCap);
+                    cachedLabelMouseover = stringBuilder.ToString();
+                }
+
+                return cachedLabelMouseover;
+            }
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            if (Current.ProgramState != ProgramState.Playing || respawningAfterLoad)
+                return;
+        }
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            Blight firstBlight = Position.GetFirstBlight(Map);
+            base.DeSpawn(mode);
+            firstBlight?.Notify_PlantDeSpawned();
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (!this.IsHashIntervalTick(2000))
+                return;
+            TickLong();
+        }
+
+        public override void TickLong()
+        {
+            if (Destroyed)
+                return;
+            base.TickLong();
+
+            cachedLabelMouseover = (string)null;
+            DoDropLeaves();
+        }
+
+
+        private void DoDropLeaves()
+        {
+            if (!true ||
+                !(MoteMaker.MakeStaticMote(Vector3.zero, Map, ThingDefOf.Mote_Leaf) is MoteLeaf moteLeaf))
+                return;
+
+            float num1 = def.graphicData.drawSize.x;
+            Vector3 vector3 = Rand.InsideUnitCircleVec3 * LeafSpawnRadius;
+            Vector3 position = Position.ToVector3Shifted() +
+                               Vector3.up * Rand.Range(LeafSpawnYMin, LeafSpawnYMax) + vector3 +
+                               Vector3.forward * def.graphicData.shadowData.offset.z;
+            double num2 = (double)Rand.Value * (double)2000.TicksToSeconds();
+            int num3 = (double)vector3.z > 0.0 ? 1 : 0;
+            double num4 = (double)num1;
+            moteLeaf.Initialize(position, (float)num2, num3 != 0, (float)num4);
+        }
+
+        public override void Print(SectionLayer layer)
+        {
+            var defPlant = def.GetModExtension<FakePlantExtension>();
+
+            Vector3 vector3 = this.TrueCenter();
+            Rand.PushState();
+            Rand.Seed = Position.GetHashCode();
+            float num3 = def.graphicData.drawSize.x * defPlant.visualSize;
+            bool singlePlantWasOfset = false;
+
+            int[] positionIndices = PlantPosIndices.GetPositionIndices(this);
+            for (int index = 0; index < positionIndices.Length; index++)
+            {
+                int num5 = positionIndices[index];
+                Vector3 center;
+                if (defPlant.maxMeshCount == 1)
+                {
+                    center = vector3 + Gen.RandomHorizontalVector(0.05f);
+                    float z = Position.z;
+                    if (center.z - defPlant.visualSize / 2.0 < z)
+                    {
+                        center.z = z + defPlant.visualSize / 2f;
+                        singlePlantWasOfset = true;
+                    }
+                }
+                else
+                {
+                    int countPerSide = 1;
+                    switch (defPlant.maxMeshCount)
+                    {
+                        case 1:
+                            countPerSide = 1;
+                            break;
+                        case 4:
+                            countPerSide = 2;
+                            break;
+                        case 9:
+                            countPerSide = 3;
+                            break;
+                        case 16:
+                            countPerSide = 4;
+                            break;
+                        case 25:
+                            countPerSide = 5;
+                            break;
+                        default:
+                            Log.Error(def + " must have plant.MaxMeshCount that is a perfect square.");
+                            break;
+                    }
+
+                    float num7 = 1f / countPerSide;
+                    center = Position.ToVector3();
+                    center.y = def.Altitude;
+                    center.x += 0.5f * num7;
+                    center.z += 0.5f * num7;
+                    center.x += (int)(num5 / countPerSide) * num7;
+                    center.z += (num5 % countPerSide) * num7;
+                    float max = num7 * 0.3f;
+                    center += Gen.RandomHorizontalVector(max);
+                }
+
+                bool flipUv = Rand.Bool;
+                Material material = Graphic.MatSingle;
+                Vector2[] uvs;
+                Graphic.TryGetTextureAtlasReplacementInfo(material, def.category.ToAtlasGroup(), flipUv, false,
+                    out material, out uvs, out Color32 _);
+                SetWindExposureColors(workingColors, this);
+                Vector2 size = new Vector2(num3, num3);
+                Printer_Plane.PrintPlane(layer, center, size, material, flipUv: flipUv, uvs: uvs,
+                    colors: workingColors, topVerticesAltitudeBias: 0.1f,
+                    uvzPayload: this.HashOffset() % 1024);
+            }
+
+
+            if (def.graphicData.shadowData != null)
+            {
+                Vector3 center = vector3 + def.graphicData.shadowData.offset * defPlant.visualSize;
+                if (singlePlantWasOfset)
+                    center.z = Position.ToVector3Shifted().z + def.graphicData.shadowData.offset.z;
+                center.y -= 0.04054054f;
+                Vector3 volume = def.graphicData.shadowData.volume * defPlant.visualSize;
+                Printer_Shadow.PrintShadow(layer, center, volume, Rot4.North);
+            }
+
+            Rand.PopState();
+        }
+
+        public static void SetWindExposureColors(Color32[] colors, FakePlant plant)
+        {
+            colors[1].a = colors[2].a = GetWindExposure(plant);
+            colors[0].a = colors[3].a = (byte)0;
+        }
+
+        public static byte GetWindExposure(FakePlant plant)
+        {
+            var defPlant = plant.def.GetModExtension<FakePlantExtension>();
+            return (byte)Mathf.Min((float)byte.MaxValue * defPlant.topWindExposure, (float)byte.MaxValue);
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            string str = InspectStringPartsFromComps();
+            if (!str.NullOrEmpty())
+                stringBuilder.Append(str);
+            return stringBuilder.ToString().TrimEndNewlines();
+        }
+
+
+        public static class PlantPosIndices
+        {
+            private static int[][][] rootList = new int[25][][];
+            private const int ListCount = 8;
+
+            static PlantPosIndices()
+            {
+                for (int index1 = 0; index1 < 25; ++index1)
+                {
+                    rootList[index1] = new int[8][];
+                    for (int index2 = 0; index2 < 8; ++index2)
+                    {
+                        int[] numArray = new int[index1 + 1];
+                        for (int index3 = 0; index3 < index1; ++index3)
+                            numArray[index3] = index3;
+                        ((IList<int>)numArray).Shuffle<int>();
+                        rootList[index1][index2] = numArray;
+                    }
+                }
+            }
+
+            public static int[] GetPositionIndices(FakePlant p)
+            {
+                var defPlant = p.def.GetModExtension<FakePlantExtension>();
+                int maxMeshCount = defPlant.maxMeshCount;
+                int index = (p.thingIDNumber ^ 42348528) % 8;
+                return rootList[maxMeshCount - 1][index];
+            }
+        }
+    }
+}
